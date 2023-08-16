5ed4d69a00790fbcee649d48b86829e92b65c9b7
sepehr <sep7696@gmail.com>
2023-08-16 09:57:31 +0330

    Initializing Dispacher .

---
+++ CQRS.Core/Infrastuctur/ICommandDispatcher.cs
@@ -0,0 +1,18 @@
+﻿using CQRS.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Infrastuctur
+{
+    public interface ICommandDispatcher
+    {
+        //register handler method that useess generic with input paramter generic and output Task 
+        // the delegate  is for our command pass in a handler METHOD WICH WILL B E ASYNC TASK
+        void Register<T>(Func<T, Task> handler) where T : BaseCommand;
+        // aloow us send dispatch our command objecct 
+        Task SendAsync(BaseCommand command);
+    }
+}
---
+++ Employee.Cmd.Infrastructure/Dispatcher/CommandDispatcher.cs
@@ -0,0 +1,37 @@
+﻿using CQRS.Core.Commands;
+using CQRS.Core.Infrastuctur;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Cmd.Infrastructure.Dispatcher
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        // filead holding all of our Commands 
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handler = new Dictionary<Type, Func<BaseCommand, Task>>();
+        public void Register<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if(_handler.ContainsKey(typeof(T)))
+            {
+                throw new Exception("Multiple commands");
+            }
+            _handler.Add(typeof(T), x => handler((T)x));
+            
+        }
+
+        public async Task SendAsync(BaseCommand command)
+        {
+            if(_handler.TryGetValue(command.GetType() , out Func<BaseCommand , Task> handler))
+            {
+                await handler(command);
+            }
+            else
+            {
+                throw new Exception("Commands not registerd");
+            }
+        }
+    }
+}
