440719a9903e689208148ff91099f0a2fec155f3
sepehr <sep7696@gmail.com>
2023-08-19 11:02:02 +0330

    AggregateRoot Initialized, Employee Aggregate IS DONE.

---
+++ CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,46 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Domain
+{
+    public abstract class AggregateRoot
+    {
+        protected Guid _id;
+        readonly List<BaseEvent> _changes = new();
+        public Guid Id
+        {
+            get { return _id; }
+        }
+        public int Version { get; set; } = -1;
+        public IEnumerable<BaseEvent> getUncommitedChanges { get { return _changes;  } }
+        public void MarkchangesAsCommit()
+        {
+            _changes.Clear();
+        }
+        private void ApplyChanges(BaseEvent @event , bool isNew)
+        {
+            var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+            if (method is null)
+                throw new ArgumentException(nameof(method), $"the Apply method not found");
+            method.Invoke(this, new object[] { @event });
+            if (isNew)
+                _changes.Add(@event);
+
+        }
+        protected void RaiseEvent(BaseEvent @event)
+        {
+            ApplyChanges(@event, true);
+        }
+        public void RelayEvent(IEnumerable<BaseEvent> events)
+        {
+            foreach(var eve in events)
+            {
+                ApplyChanges(eve, true);
+            }
+        }
+    }
+}
---
+++ Employee.Cmd.Domain/Aggregate/EmployeeAggregate.cs
@@ -0,0 +1,116 @@
+﻿using CQRS.Core.Domain;
+using Employee.Common.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Cmd.Domain.Aggregate
+{
+    public class EmployeeAggregate : AggregateRoot
+    {
+        private bool _active;
+        private string _name;
+        private readonly Dictionary<Guid,  Tuple<int, DateTime>> _vacation = new();
+        public bool Active
+        {
+            get => _active; set => _active = value;
+        }
+        public EmployeeAggregate()
+        {
+
+        }
+        public void EmployeeAggreagate(Guid id, string name, string Department)
+        {
+            RaiseEvent(new EmployeeCreatedEvent
+            {
+                Id = id,
+                Department = Department,
+                Name = name,
+                CreatedDateTime = DateTime.Now
+            });
+        }
+        public void Apply(EmployeeCreatedEvent @event)
+        {
+            _id = @event.Id;
+            _active = true;
+            _name = @event.Name;
+        }
+        public void EditEmployee(string department)
+        {
+            if (!_active)
+            {
+                throw new Exception("Yo can not Edit");
+            }
+            if (string.IsNullOrWhiteSpace(department))
+            {
+                throw new Exception("input is null");
+            }
+            RaiseEvent(new UpdateEmployeeEvent
+            {
+                Id = _id,
+                Department = department
+            });
+
+        }
+        public void Apply(UpdateEmployeeEvent @event)
+        {
+            _id = @event.Id;
+        }
+        public void DayAtWork()
+        {
+            if (!_active)
+            {
+                throw new Exception("Yo can not Add Days");
+            }
+            RaiseEvent(new DayWorkEvent
+            {
+                Id = _id
+            });
+        }
+        public void Apply(DayWorkEvent @event)
+        {
+            _id = @event.Id;
+        }
+        public void AddVacation(int TotalDays, DateTime StartDate, DateTime CreatedDate)
+        {
+            if (!_active)
+            {
+                throw new Exception("Yo can not Add vaccation");
+            }
+            RaiseEvent(new AddVacationEvent
+            {
+                Id = _id,
+                StartDate = StartDate,
+                CreatedDate = CreatedDate,
+                TotalDays = TotalDays
+            });
+        }
+        public void Apply(AddVacationEvent @event)
+        {
+            _id = @event.Id;
+            _vacation.Add(@event.Id, new Tuple<int, DateTime> ( @event.TotalDays, @event.StartDate ));
+    
+        }
+        public void DeleteEmployee(DeleteEmployeeEvent @event)
+        {
+            if (!_active)
+            {
+                throw new Exception("Yo can not remove Employye");
+            }
+            RaiseEvent(new DeleteEmployeeEvent
+            {
+                Id = _id,
+                DeletedDateTime= @event.DeletedDateTime,
+                Name= @event.Name
+            });
+        }
+        public void Apply(DeleteEmployeeEvent @event)
+        {
+            _id = @event.Id;
+        }
+    }//AddVacationEvent
+   
+}
