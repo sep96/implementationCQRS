b9e4d8c0644728209ea4d0f616d528b58240f444
sepehr <sep7696@gmail.com>
2023-08-22 09:38:39 +0330

    EmployeeEntity , VacationEntity is initilized.

---
+++ Employee.Query.Domain/Entity/EmployeeEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Query.Domain.Entity
+{
+    [Table("Employee")]
+    public  class EmployeeEntity
+    {
+        [Key]
+        public Guid EmmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int DayofWorks { get; set; }
+        public virtual ICollection<VacationEntity> Vacations { get; set; }
+
+    }
+}
---
+++ Employee.Query.Domain/Entity/VacationEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Employee.Query.Domain.Entity
+{
+    [Table("Vacation")]
+    public class VacationEntity
+    {
+        public Guid VacationId { get; set; }
+        public int TotalDays { get; set; }
+
+        public DateTime StartDate { get; set; }
+        [JsonIgnore]
+        public virtual EmployeeEntity Employee { get; set; }
+
+    }
+}
