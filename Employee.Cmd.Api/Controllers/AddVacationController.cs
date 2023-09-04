79d9c55bac94bbe09ccf644445bf9de31183035f
sepehr <sep7696@gmail.com>
2023-09-04 10:37:15 +0330

    Vacation , dayWork , Deleted Controllers ins Finished.

---
+++ Employee.Cmd.Api/Controllers/AddVacationController.cs
@@ -0,0 +1,56 @@
+﻿using CQRS.Core.Infrastuctur;
+using Employee.Cmd.Api.Commands;
+using Employee.Cmd.Api.Dtos;
+using Employee.Common.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Cmd.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddVacationController : ControllerBase
+    {
+        private ILogger<AddVacationController> _logger;
+        private ICommandDispatcher _commandDispatcher;
+
+        public AddVacationController(ILogger<AddVacationController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+        [HttpPatch("{id}")]
+        public virtual async Task<ActionResult> AddVacationCommandAsync(Guid id, AddVacationCommand command)
+        {
+            command.Id = id;
+            try
+            {
+                await _commandDispatcher.SendAsync(command);
+                return StatusCode(StatusCodes.Status200OK, new NewEmployeeResponse
+                {
+                    Id = command.Id,
+                    Message = "Employee Deleted Successfully"
+                });
+            }
+            catch (InvalidOperationException sd)
+            {
+                _logger.Log(LogLevel.Warning, "Client had bad request");
+                return BadRequest(new BaseResponse
+                {
+                    Message = sd.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSSAGE = "internal Error";
+                _logger.Log(LogLevel.Error, ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new NewEmployeeResponse
+                {
+                    Message = SAFE_ERROR_MESSSAGE
+                    ,
+                    Id = id
+                });
+            }
+        }
+    }
+}
---
+++ Employee.Cmd.Api/Controllers/DaysWorkController.cs
@@ -0,0 +1,56 @@
+﻿using CQRS.Core.Infrastuctur;
+using Employee.Cmd.Api.Commands;
+using Employee.Cmd.Api.Dtos;
+using Employee.Common.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Cmd.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DaysWorkController : ControllerBase
+    {
+        private ILogger<DaysWorkController> _logger;
+        private ICommandDispatcher _commandDispatcher;
+
+        public DaysWorkController(ILogger<DaysWorkController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+        [HttpPatch("{id}")]
+        public virtual async Task<ActionResult> DaysWorkCommandAsync(Guid id, DaysWorkCommand command)
+        {
+            command.Id = id;
+            try
+            {
+                await _commandDispatcher.SendAsync(command);
+                return StatusCode(StatusCodes.Status200OK, new NewEmployeeResponse
+                {
+                    Id = command.Id,
+                    Message = "Employee Deleted Successfully"
+                });
+            }
+            catch (InvalidOperationException sd)
+            {
+                _logger.Log(LogLevel.Warning, "Client had bad request");
+                return BadRequest(new BaseResponse
+                {
+                    Message = sd.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSSAGE = "internal Error";
+                _logger.Log(LogLevel.Error, ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new NewEmployeeResponse
+                {
+                    Message = SAFE_ERROR_MESSSAGE
+                    ,
+                    Id = id
+                });
+            }
+        }
+    }
+}
