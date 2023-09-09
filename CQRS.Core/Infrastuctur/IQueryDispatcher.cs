84d397b690d36ff004aa6fb829c1be3b541e39dc
sepehr <sep7696@gmail.com>
2023-09-09 15:43:14 +0330

    IQuery Just Created.

---
+++ CQRS.Core/Infrastuctur/IQueryDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.Core.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Infrastuctur
+{
+    public interface IQueryDispatcher<TEntity>
+    {
+        void RegisterHandler<TQuery>(Func<TQuery, Task<List<TEntity>>> handler)where TQuery:BaseQuery;
+        Task<List<TEntity>> SendAsync(BaseQuery query);
+    }
+}
